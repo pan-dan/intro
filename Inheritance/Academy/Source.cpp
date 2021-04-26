daf7447cf5233624de8f03c1d42514173e7ca466
pan_dan <ad_ar@list.ru>
2021-04-26 19:15:34 +0500

    Graduate DONE

--- Inheritance/Academy/Source.cpp
+++ Inheritance/Academy/Source.cpp
@@ -7,7 +7,16 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+
 #define DEL "--------------------------------------------------------"
+
+#define HUMAN_TAKE_PARAMETERS		const string& last_name, const string& first_name, unsigned int age
+#define STUDENT_GET_PARAMETERS		const string& university, const string& speciality, const string& group, double rating	
+#define TEACHER_GET_PARAMETERS		const string& speciality, unsigned int experiance
+
+#define HUMAN_GIVE_PARAMETERS		last_name, first_name,age
+#define STUDENT_GIVE_PARAMETERS		university,speciality,group,rating
+#define TEACHER_GIVE_PARAMETERS		speciality, experience
 
 
 class Human
@@ -88,9 +97,14 @@ class Student :public Human
 	string speciality;
 	string group;
 	double rating;
+	string university;
 
 public:
 
+	const string& get_university()const
+	{
+		return university;
+	}
 	const string& get_speciality()const
 	{
 		return speciality;
@@ -103,7 +117,12 @@ public:
 	{
 		return rating;
 	}
+	
 
+	void set_university(const string& university)
+	{
+		this->university = university;
+	}
 	void set_speciality(const string& speciality)
 	{
 		this->speciality = speciality;
@@ -116,16 +135,17 @@ public:
 	{
 		this->rating = rating;
 	}
-
+	
 
 
 	//			Constructors
 	Student
 	(
-	const string& last_name, const string& first_name, unsigned int age,	// Атрибуты базового класса
-	const string& speciality, const string& group, double rating			// Атрибуты нашего класса
-	):Human(last_name, first_name,age)
+	HUMAN_TAKE_PARAMETERS,							// Атрибуты базового класса
+	STUDENT_GET_PARAMETERS			// Атрибуты нашего класса
+	):Human(HUMAN_GIVE_PARAMETERS)
 	{
+		set_university(university);
 		set_speciality(speciality);
 		set_group(group);
 		set_rating(rating);
@@ -143,7 +163,7 @@ public:
 	void info()const
 	{
 		Human::info();
-		cout << speciality << ", " << group << ", успеваемость: " << rating << endl;
+		cout << university << ", " << speciality << ", " << group << ", успеваемость: " << rating << ", "<< endl;
 	}
 
 
@@ -181,9 +201,9 @@ public:
 	//			Constructors
 	Teacher
 	(
-		const string& last_name, const string& first_name, unsigned int age,
-		const string& speciality, unsigned int experiance
-	):Human(last_name, first_name, age)
+		HUMAN_TAKE_PARAMETERS,
+		TEACHER_GET_PARAMETERS
+	):Human(HUMAN_GIVE_PARAMETERS)
 	{
 		set_speciality(speciality);
 		set_experiance(experiance);
@@ -201,25 +221,89 @@ public:
 	void info()
 	{
 		Human::info();
-		cout << spesiality << ", опыт предподавания: " << experiance << " лет." << endl;
+		cout << spesiality << ", опыт предподавания: '" << experiance << " лет." << endl;
 			
 	}
 
 };
 
+class Graduate:public Student
+{
+	string diploma;					//Красный диплом или обычный
+	string topic;					//Тема дипломного проекта
+
+
+public: 
+
+	const string& get_diploma()const
+	{
+		return diploma;
+	}
+	const string& get_topic()const
+	{
+		return topic;
+	}
+
+
+	void set_diploma(const string& diploma)
+	{
+		this->diploma = diploma;
+	}
+	void set_topic(const string& topic)
+	{
+		this->topic = topic;
+	}
+
+	//			Constructors
+	Graduate
+	(
+		HUMAN_TAKE_PARAMETERS,
+		STUDENT_GET_PARAMETERS,
+		const string& diploma, const string& topic
+	):Student(HUMAN_GIVE_PARAMETERS,STUDENT_GIVE_PARAMETERS), diploma(diploma), topic(topic)
+	{
+
+		cout << "GConstructor:\t" << this << endl;
+
+	}
+	
+	~Graduate()
+	{
+		cout << "GDestructor" << this << endl;
+	}
+
+	//			Methods
+	void info()const
+	{
+		Student::info();
+		cout << diploma << " диплом, " << "тема проекта: " << topic << "." << endl;
+	}
+
+
+};
+
+
+#define INHERITANCE_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "Rus");
 
+#ifdef INHERITANCE_CHECK
 	/*Human human("Тупенко", "Василий", 35);
-	cout << DEL << endl;
-	human.info();
-	cout << DEL << endl;*/
+cout << DEL << endl;
+human.info();
+cout << DEL << endl;*/
 
-	Student vasya("Тупенко", "Василий", 18, "Программирование", "BV_011", 4.5);
+	Student vasya("Тупенко", "Василий", 18, "УРФО", "Программирование", "BV_011", 4.5);
 	vasya.info();
 
 	Teacher teacher("Einstein", "Albert", 150, "Phisics", 120);
 	teacher.info();
+
+	Graduate Tony("Montana", "Antonio", 25, "МГУ", "Современные технологии продаж",
+		"BV_011", 80, "Красный", "Распространение кокаина");
+	Tony.info();
+#endif // INHERITANCE_CHECK
+
 }
