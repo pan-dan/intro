09f114647b28e025cc2b3e95fc27d287c79ab101
pan_dan <ad_ar@list.ru>
2021-06-18 20:13:34 +0500

    Element methods out of class

--- DataContainers/List/Source.cpp
+++ DataContainers/List/Source.cpp
@@ -23,23 +23,8 @@ class List
 		Element* pPrev;	//”казатель на предыдущий элемент
 
 	public:
-		Element(T Data, Element* pNext = nullptr, Element* pPrev = nullptr)
-		{
-			this->Data = Data;
-			this->pNext = pNext;
-			this->pPrev = pPrev;
-#ifdef DEBUG
-			cout << "EConstructor:\t" << this << endl;
-#endif // DEBUG
-
-		}
-		~Element()
-		{
-#ifdef DEBUG
-			cout << "EDestructor:\t" << this << endl;
-#endif // DEBUG
-
-		}
+		Element(T Data, Element* pNext = nullptr, Element* pPrev = nullptr);
+		~Element();
 
 		friend class List<T>;
 	};
@@ -229,6 +214,24 @@ public:
 	void print_reverse();
 
 };
+
+template <typename T> List<T>::Element::Element(T Data, Element* pNext, Element* pPrev)
+{
+	this->Data = Data;
+	this->pNext = pNext;
+	this->pPrev = pPrev;
+#ifdef DEBUG
+	cout << "EConstructor:\t" << this << endl;
+#endif // DEBUG
+
+}
+template <typename T> List<T>::Element::~Element()
+{
+#ifdef DEBUG
+	cout << "EDestructor:\t" << this << endl;
+#endif // DEBUG
+
+}
 
 template <typename T>size_t List<T>::getSize()const
 {
